c3a4ced8d2d363df5f1dbe9cb8fe6f25b024a196
ayrus241 <53077799+ayrus241@users.noreply.github.com>
2019-07-23 06:36:59 +0000

    Update guvi_9.py

--- guvi_9.py
+++ guvi_9.py
@@ -1,16 +1,18 @@
-k=int(input())
-n=int(input())
+k,n=((input().split()))
 a=list(input())
 i=0
-while(i<n):
+while(i<int(n)):
     o=a[i]
     i=i+1
 i=0
 x=0
-while(i<n):
+while(i<int(n)):
     x=x+int(a[i])
     i=i+1
 print(x)
+    
+    
+
     
     
 
